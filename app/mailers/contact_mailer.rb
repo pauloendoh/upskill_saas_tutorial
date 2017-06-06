120c420ac4b126bf240e333cf7f00ae896b18799
Paulo Endoh <paulo.endoh@gmail.com>
2017-06-06 00:39:23 +0000

    Added contact form mailer view

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'instructors@upskillcourses.com'
+  default to: 'paulo.endoh@gmail.com'
   
   def contact_email(name, email, body)
     @name = name
